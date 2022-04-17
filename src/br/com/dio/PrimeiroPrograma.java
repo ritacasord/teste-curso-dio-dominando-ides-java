cd09e0e2d507da346b28481d6e6d4e148a4e1b93
ritacasord <rita_cas_@hotmail.com>
2022-04-17 15:03:50 -0300

    first commit
    Signed-off-by: ritacasord <rita_cas_@hotmail.com>

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro ( );	
+		Gato gato = new Gato( );
+				
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		
+		/*int a = 2;
+		int b = 3;		
+		// TODO Auto-generated method stub
+		System.out.println("Hello World " + ( a+ b)); */
+	
+	}
+
+}
+
+class Livro {
+	public String livro;
+	public Integer numPaginas;
+}
+
